aba63eae77ecd47e9a33360a0b9c0ee429689b45
nt-tnnhan <104969130+nt-tnnhan@users.noreply.github.com>
2022-06-13 11:51:07 +0700

    demoXLM

---
+++ src/demoXML/JsonDemo.java
@@ -0,0 +1,12 @@
+package demoXML;
+
+public class JsonDemo {
+	public static void main(String[] args) {
+		JSONObject jo = new JSONObject();
+		
+		jo.put("username", "ncuy");
+		jo.put("password", "ncuy");
+		System.out.println(jo);
+		System.out.println(jo.get("username"));
+	}
+}
